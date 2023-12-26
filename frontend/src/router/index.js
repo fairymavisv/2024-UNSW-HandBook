03c9583ab8a535b02e0aaf9b8d1844665042198f
tron <c1nenfan@gmail.com>
2023-12-26 16:33:05 +0800

    add create user profile(program and major)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue';
+import RegisterView from '../views/RegisterView.vue';
 
 const routes = [
+
+  {
+    path: '/',
+    name: 'login',
+    component: LoginView
+  },
+
+  {
+    path: '/signup',
+    name: 'signup',
+    component: RegisterView
+
+  },
+
+  // {
+  //   path: '/forgot-password',
+  //   name: 'ForgotPassword',
+  //   component: ForgotPassword
+  // },
+
+  {
+    path: '/home',
+    name: 'home',
+    component: HomeView
+  },
 
   {
     path: '/courseList',
